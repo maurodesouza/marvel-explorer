97d9a79febedf2a92aff65644b8d29b2b48b85c8
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-05-27 04:20:33 -0300

    ⚡  chore: change title and app description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,13 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Marvel Explorer</title>
         <link rel="shortcut icon" href="/images/icon-512.png" />
         <link rel="apple-touch-icon" href="/images/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Search for your favorite heroes with this awesome app"
         />
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
