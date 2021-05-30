e70a6f0eaf54c5a10fb5a50507dea83a02d4de90
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-05-29 22:09:37 -0300

    ⚡  chore: improve heroes grid stories

--- src/components/HeroesGrid/stories.tsx
+++ src/components/HeroesGrid/stories.tsx
@@ -1,10 +1,48 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 
-import HeroesGrid from '.';
+import HeroCard from '../HeroCard';
+
+import HeroesGrid, { HeroesGridProps } from '.';
+
+type HeroesGridStoryProps = HeroesGridProps & {
+  renderHeroCards: number;
+};
+
+const props = {
+  heroId: 123,
+  heroName: 'Iron man',
+  heroThumbnail:
+    'http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55.jpg',
+};
 
 export default {
   title: 'HeroesGrid',
   component: HeroesGrid,
-} as Meta;
 
-export const Basic: Story = args => <HeroesGrid {...args} />;
+  argTypes: {
+    theme: { table: { disable: true } },
+    ref: { table: { disable: true } },
+    as: { table: { disable: true } },
+    forwardedAs: { table: { disable: true } },
+  },
+
+  args: {
+    renderHeroCards: 1,
+  },
+} as Meta<HeroesGridStoryProps>;
+
+const createArray = (lenght: number): string[] => new Array(lenght).fill('');
+
+export const Basic: Story<HeroesGridStoryProps> = args => {
+  const { renderHeroCards, ...rest } = args;
+
+  return (
+    <div style={{ width: '100%', maxWidth: '1040px' }}>
+      <HeroesGrid {...rest}>
+        {createArray(renderHeroCards).map((_, index) => (
+          <HeroCard key={props.heroId * (index + 1)} {...props} />
+        ))}
+      </HeroesGrid>
+    </div>
+  );
+};
