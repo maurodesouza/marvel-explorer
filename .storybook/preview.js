7e34cf3ceda81b4656d61e08340c858eb214c5c2
Mauro de Souza <maurodesouza2017@hotmail.com>
2021-05-30 23:08:32 -0300

    🐛  fix: error in stories that use useRouter hook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,13 @@
+import { addDecorator } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router';
+
 import { ThemeProvider } from 'styled-components';
 import { FavoriteHeroesProvider } from 'hooks/useFavoriteHeroes';
 
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme/default';
+
+addDecorator(withNextRouter());
 
 export const decorators = [
   (Story) => (
